82637f7bb8094e255a6a33ba25cd6d68bba69add
carlescamig <72892561+carlescamig@users.noreply.github.com>
2025-05-20 14:02:10 -0300

    add pulumi backend

--- index.ts
+++ index.ts
@@ -3,11 +3,8 @@ import * as pulumi from "@pulumi/pulumi";
 import { createPipeline } from "./lib/pipeline";
 import { registerAutoTags } from "./lib/autotag";
 import { appName, awsRegion, environment } from "./lib/config";
-import { deployProjectInfrastructure } from "./lib/projectInfra";
 import { name } from "./lib/utils/naming";
-import("./lib/projectInfra");
-// Obtener nombre del stack actual (ej: "dev", "prod", "pipeline")
-const stack = pulumi.getStack();
+
 const config = new pulumi.Config();
 // Aplica tags automáticos
 registerAutoTags({
@@ -21,64 +18,68 @@ registerAutoTags({
   "user:Team": "strata-analytics-team",
 });
 
-if (stack === "pipeline") {
-  // 👉 Stack destinado a desplegar el pipeline
-    const pulumiTokenSecret = new aws.secretsmanager.Secret(
-      name("pulumi-token"),
-      {
-        name: "PULUMI_ACCESS_TOKEN",
-      }
-    );
-  
-    new aws.secretsmanager.SecretVersion(name("pulumi-token-version"), {
-      secretId: pulumiTokenSecret.id,
-      secretString: config.requireSecret("PULUMI_ACCESS_TOKEN"),
-    });
-  
-    createPipeline({
-      fullRepositoryId: config.require("fullRepositoryId"),
-      branch: config.require("branch"),
-      providerType: config.require("providerType"),
-      stages: [
-        {
-          name: "pulumi-dev",
-          manualApproval: false,
-          build: {
-            buildspec: "buildspec.yml",
-            environmentVariables: [
-              {
-                name: "PULUMI_ACCESS_TOKEN",
-                value: "PULUMI_ACCESS_TOKEN",
-                type: "SECRETS_MANAGER",
-              },
-              {
-                name: "PULUMI_STACK",
-                value: "dev",
-              },
-            ],
+// 👉 Stack destinado a desplegar el pipeline
+// Recurso para almacenar el estado de Pulumi en un bucket S3
+const pulumiState = new aws.s3.Bucket("pulumi-state", {
+  bucket: "my-pulumi-state-storage",
+  acl: "private",
+  tags: {
+    Name: "PulumiStateStorage",
+    Environment: "Dev",
+  },
+});
+
+const pulumiTokenSecret = new aws.secretsmanager.Secret(name("pulumi-token"), {
+  name: "PULUMI_ACCESS_TOKEN",
+});
+
+new aws.secretsmanager.SecretVersion(name("pulumi-token-version"), {
+  secretId: pulumiTokenSecret.id,
+  secretString: config.requireSecret("PULUMI_ACCESS_TOKEN"),
+});
+
+createPipeline({
+  fullRepositoryId: config.require("fullRepositoryId"),
+  branch: config.require("branch"),
+  providerType: config.require("providerType"),
+  stages: [
+    {
+      name: "pulumi-dev",
+      manualApproval: false,
+      build: {
+        buildspec: "buildspec.yml",
+        environmentVariables: [
+          {
+            name: "PULUMI_ACCESS_TOKEN",
+            value: "PULUMI_ACCESS_TOKEN",
+            type: "SECRETS_MANAGER",
           },
-        },
-        {
-          name: "pulumi-prod",
-          manualApproval: true,
-          build: {
-            buildspec: "buildspec.yml",
-            environmentVariables: [
-              {
-                name: "PULUMI_ACCESS_TOKEN",
-                value: "PULUMI_ACCESS_TOKEN",
-                type: "SECRETS_MANAGER",
-              },
-              {
-                name: "PULUMI_STACK",
-                value: "prod",
-              },
-            ],
+          {
+            name: "PULUMI_STACK",
+            value: "dev",
           },
-        },
-      ],
-    });
-} else {
-  // 👉 Stack de infraestructura del proyecto real (dev/prod/etc)
-  deployProjectInfrastructure(stack);
-}
+        ],
+      },
+    },
+    {
+      name: "pulumi-prod",
+      manualApproval: true,
+      build: {
+        buildspec: "buildspec.yml",
+        environmentVariables: [
+          {
+            name: "PULUMI_ACCESS_TOKEN",
+            value: "PULUMI_ACCESS_TOKEN",
+            type: "SECRETS_MANAGER",
+          },
+          {
+            name: "PULUMI_STACK",
+            value: "prod",
+          },
+        ],
+      },
+    },
+  ],
+});
+
+export const pulumiBucketName = pulumiState.bucket;
